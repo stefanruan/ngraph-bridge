dc40b09becc0002bc2d22da100190c1e54c8d8be
sindhu-nervana <sindhura.kantamani@intel.com>
2019-07-26 10:57:32 -0700

    more changes

--- test/python/test_bfloat16.py
+++ test/python/test_bfloat16.py
@@ -31,37 +31,36 @@ def model2(dtype):
     return tf.matmul(x, y), [x, y]
 
 
-def get_config(nativerun):
-    config = tf.ConfigProto(
-        allow_soft_placement=True,
-        log_device_placement=False,
-        inter_op_parallelism_threads=1)
-    if nativerun:
-        return config
-    else:
-        return ngraph_bridge.update_config(config)
-
+config = tf.ConfigProto(
+    allow_soft_placement=True,
+    log_device_placement=False,
+    inter_op_parallelism_threads=1)
 
 outputs = []
 k_np = np.random.rand(3, 3)
-for dtype, nativerun in [(tf.float32, True), (tf.float32, False)]:
-    if nativerun:
-        with tf.Session(config=get_config(nativerun)) as sess1:
-            ngraph_bridge.disable()
-            out, list_of_ins = model1(dtype)
-            out = tf.cast(out, dtype=tf.float32)
-            feed_dict = {k: k_np for k in list_of_ins}
-            outval = sess1.run(out, feed_dict=feed_dict)
-            outputs.append(outval[0][0])
-    else:
-        with tf.Session(config=get_config(nativerun)) as sess2:
-            ngraph_bridge.enable()
-            os.environ['NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS'] = '1'
-            os.environ['NGRAPH_TF_BACKEND'] = 'NNP'
-            out, list_of_ins = model2(dtype)
-            feed_dict = {k: k_np for k in list_of_ins}
-            outval = sess2.run(out, feed_dict=feed_dict)
-            outputs.append(outval[0][0])
-    print(out.dtype, nativerun, outval[0][0])
+
+#Test 1: model1 TF-native
+with tf.Session(config=config) as sess1:
+    ngraph_bridge.disable()
+    out, list_of_ins = model1(dtype=tf.float32)
+    out = tf.cast(out, dtype=tf.float32)
+    feed_dict = {k: k_np for k in list_of_ins}
+    outval = sess1.run(out, feed_dict=feed_dict)
+    print("Native TF: ")
+    print(out.dtype, outval[0][0])
+    outputs.append(outval[0][0])
+
+#Test 2: model2 with ngraph, NNP backend
+with tf.Session(config=config) as sess2:
+    ngraph_bridge.enable()
+    ngraph_bridge.update_config(config)
+    os.environ['NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS'] = '1'
+    os.environ['NGRAPH_TF_BACKEND'] = 'NNP'
+    out, list_of_ins = model2(dtype=tf.float32)
+    feed_dict = {k: k_np for k in list_of_ins}
+    outval = sess2.run(out, feed_dict=feed_dict)
+    print("Ngraph with NNP backend: ")
+    print(out.dtype, outval[0][0])
+    outputs.append(outval[0][0])
 
 assert np.allclose(outputs[0], outputs[1])
