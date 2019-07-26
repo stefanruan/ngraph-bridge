27ce47264bf2bf1b446aeb8c3b7ca5c0a51c3ab9
Shrestha Malik <shrestha.malik@intel.com>
2019-07-26 13:53:04 -0700

    Minor Changes to the test

--- test/python/test_bfloat16.py
+++ test/python/test_bfloat16.py
@@ -5,30 +5,33 @@ import os
 
 # Tests the scenario below
 # Reference: NativeTF + CPU
-# - Input Tensor A,B,C,... : Dt.Float
-# - Cast A,B,C,,,, to bfloat16
-# - Run with TF
-# - Output X
-# This needs to be converted back to float32 for comparison
-# Actual : TF with nGraph, NNP, Bo Funcsim
-# - Same input tensors A,B,C : Dt.Float
-# - Run with TF Bridge
-# - Output Y
-# - Do X and Y match with our reference tolerance (nnp.allclose/C++ Compare)
+# - Input Tensor X,Y ... : Dt.Float
+# - Cast X,Y to bfloat16
+# - MatMul takes in X,Y->outputs A (bfloat16)
+# - Cast A to Dt.Float
+
+# Actual : TF with nGraph, NNP, B0 Funcsim
+# - Same input tensors X,Y : Dt.Float
+# -  MatMul takes in X,Y ->outputs B (Dt.Float)
+
+# Compare A and B with tolerance
 
 
-def model1(dtype):
-    x = tf.placeholder(dtype, [3, 3], name='x')
-    y = tf.placeholder(dtype, [3, 3], name='y')
+def tf_model():
+    x = tf.placeholder(tf.float32, [3, 3], name='x')
+    y = tf.placeholder(tf.float32, [3, 3], name='y')
     x = tf.cast(x, dtype=tf.bfloat16)
     y = tf.cast(y, dtype=tf.bfloat16)
-    return tf.matmul(x, y), [x, y]
+    m = tf.matmul(x, y)
+    m = tf.cast(m, dtype=tf.float32)
+    return m, [x, y]
 
 
-def model2(dtype):
-    x = tf.placeholder(dtype, [3, 3], name='x')
-    y = tf.placeholder(dtype, [3, 3], name='y')
-    return tf.matmul(x, y), [x, y]
+def ng_model():
+    x = tf.placeholder(tf.float32, [3, 3], name='x')
+    y = tf.placeholder(tf.float32, [3, 3], name='y')
+    m = tf.matmul(x, y)
+    return m, [x, y]
 
 
 config = tf.ConfigProto(
@@ -36,31 +39,27 @@ config = tf.ConfigProto(
     log_device_placement=False,
     inter_op_parallelism_threads=1)
 
-outputs = []
 k_np = np.random.rand(3, 3)
 
-#Test 1: model1 TF-native
-with tf.Session(config=config) as sess1:
+#Test 1: tf_model TF-native
+with tf.Session(config=config) as sess_tf:
     ngraph_bridge.disable()
-    out, list_of_ins = model1(dtype=tf.float32)
-    out = tf.cast(out, dtype=tf.float32)
+    tf_out, list_of_ins = tf_model()
     feed_dict = {k: k_np for k in list_of_ins}
-    outval = sess1.run(out, feed_dict=feed_dict)
+    tf_outval = sess_tf.run(tf_out, feed_dict=feed_dict)
     print("Native TF: ")
-    print(out.dtype, outval[0][0])
-    outputs.append(outval[0][0])
+    print(tf_outval.dtype, tf_outval[0][0])
 
 #Test 2: model2 with ngraph, NNP backend
-with tf.Session(config=config) as sess2:
+with tf.Session(config=config) as sess_ng:
     ngraph_bridge.enable()
     ngraph_bridge.update_config(config)
     os.environ['NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS'] = '1'
     os.environ['NGRAPH_TF_BACKEND'] = 'NNP'
-    out, list_of_ins = model2(dtype=tf.float32)
+    ng_out, list_of_ins = ng_model()
     feed_dict = {k: k_np for k in list_of_ins}
-    outval = sess2.run(out, feed_dict=feed_dict)
+    ng_outval = sess_ng.run(ng_out, feed_dict=feed_dict)
     print("Ngraph with NNP backend: ")
-    print(out.dtype, outval[0][0])
-    outputs.append(outval[0][0])
+    print(ng_outval.dtype, ng_outval[0][0])
 
-assert np.allclose(outputs[0], outputs[1])
+assert np.allclose(tf_outval, ng_outval)
