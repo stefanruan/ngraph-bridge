adca2a15f095a2887cae170a7355d54189770657
Sayantan Sarkar <sayantan.sarkar@intel.com>
2019-12-17 14:50:25 -0800

    Empty const node fix

--- ngraph_bridge/ngraph_utils.h
+++ ngraph_bridge/ngraph_utils.h
@@ -135,25 +135,39 @@ Status ValuesFromConstNode(const NodeDef& node,
         // TODO(amprocte/NGRAPH-2502): there are more element types to support
         // here
         case DT_INT32:
-          (*values)[i] = (tensor.int_val_size() == 1 ? tensor.int_val()[0]
-                                                     : tensor.int_val()[i]);
+          (*values)[i] =
+              tensor.int_val_size() == 0
+                  ? 0
+                  : ((tensor.int_val_size() == 1 ? tensor.int_val()[0]
+                                                 : tensor.int_val()[i]));
           break;
         case DT_INT64:
-          (*values)[i] = (tensor.int64_val_size() == 1 ? tensor.int64_val()[0]
-                                                       : tensor.int64_val()[i]);
+          (*values)[i] =
+              tensor.int_val_size() == 0
+                  ? 0
+                  : ((tensor.int64_val_size() == 1 ? tensor.int64_val()[0]
+                                                   : tensor.int64_val()[i]));
           break;
         case DT_FLOAT:
-          (*values)[i] = (tensor.float_val_size() == 1 ? tensor.float_val()[0]
-                                                       : tensor.float_val()[i]);
+          (*values)[i] =
+              tensor.int_val_size() == 0
+                  ? 0.0
+                  : ((tensor.float_val_size() == 1 ? tensor.float_val()[0]
+                                                   : tensor.float_val()[i]));
           break;
         case DT_BOOL:
-          (*values)[i] = (tensor.bool_val_size() == 1 ? tensor.bool_val()[0]
-                                                      : tensor.bool_val()[i]);
+          (*values)[i] =
+              tensor.int_val_size() == 0
+                  ? false
+                  : ((tensor.bool_val_size() == 1 ? tensor.bool_val()[0]
+                                                  : tensor.bool_val()[i]));
           break;
         case DT_DOUBLE:
           (*values)[i] =
-              (tensor.double_val_size() == 1 ? tensor.double_val()[0]
-                                             : tensor.double_val()[i]);
+              tensor.int_val_size() == 0
+                  ? 0.0
+                  : ((tensor.double_val_size() == 1 ? tensor.double_val()[0]
+                                                    : tensor.double_val()[i]));
           break;
         default:
           NGRAPH_VLOG(0)
